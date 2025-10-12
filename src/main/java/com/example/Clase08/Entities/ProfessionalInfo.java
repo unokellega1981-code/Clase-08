d2d847c4230daa11d52310c7df56098420523722
JACG <jacarreragonzalez@gmail.com>
2025-10-12 17:17:10 +0200

    Creados los "entities"

---
+++ src/main/java/com/example/Clase08/Entities/ProfessionalInfo.java
@@ -0,0 +1,44 @@
+package com.example.Clase08.Entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class ProfessionalInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "worker_id")
+    private Integer workerId;
+
+    @Column(name = "qualification")
+    private String qualification;
+
+    @Column(name = "specialization")
+    private String specialization;
+
+    @Column(name = "years_of_experience")
+    private Integer yearsOfExperience;
+
+    @Column(name = "previous_company")
+    private String previousCompany;
+
+    @Column(name = "previous_job_title")
+    private String previousJobTitle;
+
+    @Column(name = "previous_job_start_date")
+    private Date previousJobStartDate;
+
+    @Column(name = "previous_job_end_date")
+    private Date previousJobEndDate;
+
+}
---
+++ src/main/java/com/example/Clase08/Entities/WorkerInfo.java
@@ -0,0 +1,65 @@
+package com.example.Clase08.Entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class WorkerInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "worker_id")
+    private Integer workerId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "middle_name")
+    private String middleName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "date_of_birth")
+    private Date dateOfBirth;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "postal_code")
+    private String postalCode;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "alternate_phone_number")
+    private String alternatePhoneNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "gender_id")
+    private Integer genderId;
+
+    @Column(name = "nationality")
+    private String nationality;
+
+}
